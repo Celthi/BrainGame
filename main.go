7251029159f860114d6ad82834b7567dfbdb2611
Ralph <Evan_l00@qq.com>
2017-10-22 11:24:40 +0800

    The base code that run the web

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	//"github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	fmt.Print("Hello world \n")
+	port := os.Getenv("PORT")
+	port = "8090"
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+	router.GET("/", func(c *gin.Context){
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+	router.Run(":" + port)
+}
